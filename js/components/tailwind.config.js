f15f7398abaf30b47ccf7ff32b360d06e1d1531c
kevincito0987 <kevinangaritacaamano@gmail.com>
2025-03-06 18:17:16 -0500

    feat: :heavy_plus_sign: Listo para usar tailwindcss para mañana onfire.

---
+++ js/components/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./views/**/*.html"], // Escanea todos los HTML dentro de "views", incluyendo index.html
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
